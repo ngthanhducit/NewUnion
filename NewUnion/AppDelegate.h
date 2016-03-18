ce042cddb1359cb47c59d04e3ed8a7faecf350ce
haclong <dev2@sycomore.vn>
2016-03-18 21:31:50 +0700

    Initial Commit

---
+++ NewUnion/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewUnion
+//
+//  Created by ChjpCoj on 18/03/16.
+//  Copyright © 2016 ChjpCoj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewUnion/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewUnion
+//
+//  Created by ChjpCoj on 18/03/16.
+//  Copyright © 2016 ChjpCoj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
